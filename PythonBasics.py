2bcf6352622f936dbba2e095c297a2c45c6f31f1
knarfortiz <o.g.knarf@hotmail.com>
2024-10-01 21:20:50 -0500

    variables

---
+++ PythonBasics.py
@@ -0,0 +1,13 @@
+"""
+Variables
+"""
+
+cost = 10
+tax_percentage = .25
+tax = cost * tax_percentage
+price = cost + tax
+print(price)
+
+username = 'john'
+first_name = 'john'
+print(f'{username} {first_name}')
