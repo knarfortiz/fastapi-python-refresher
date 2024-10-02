dc800b6009c379d8f9675dbee3fa506d53a57a7f
knarfortiz <o.g.knarf@hotmail.com>
2024-10-01 21:52:34 -0500

    comments

---
+++ Comments.py
@@ -0,0 +1,19 @@
+# Going to print hello world
+print('Hello world')
+print('Hello world 2')
+
+# This is going over
+# Multiple
+# Lines
+
+"""
+This is going
+over multiple
+lines
+"""
+
+'''
+This is going
+over multiple
+lines
+'''
