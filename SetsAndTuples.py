4c4d0d87bb10eacef67124d39d070c04b2a89a7e
knarfortiz <o.g.knarf@hotmail.com>
2024-10-16 17:07:00 -0500

    sets and tuples

--- SetsAndTuples.py
+++ SetsAndTuples.py
@@ -3,32 +3,35 @@ Sets are similar to lists but are unordered and do not allow duplicates
 use curly brackets
 """
 
-# my_sets = {1, 2, 3, 4, 5, 1, 2}
-# print(my_sets)
-# print(len(my_sets))
-#
-# for i in my_sets:
-#     print(i)
-#
-# my_sets.add(6)
-# print(my_sets)
-#
-# my_sets.discard(6)
-# print(my_sets)
-#
-# my_sets.update([7, 8, 9])
-# print(my_sets)
+my_sets = {1, 2, 3, 4, 5, 1, 2}
+print(my_sets)
+print(len(my_sets))
+
+for i in my_sets:
+    print(i)
+
+my_sets.add(6)
+print(my_sets)
+
+my_sets.discard(6)
+print(my_sets)
+
+my_sets.update([7, 8, 9])
+print(my_sets)
+
+
 
 """
 Tuples are similar to lists but are ordered
 """
 
-# my_tuples = (1, 2, 3, 4, 5)
-# print(my_tuples)
-# print(len(my_tuples))
-#
-# for i in my_tuples:
-#     print(i)
+my_tuples = (1, 2, 3, 4, 5)
+print(my_tuples)
+print(len(my_tuples))
+
+for i in my_tuples:
+    print(i)
+
 
 """
 Lists Assignment
