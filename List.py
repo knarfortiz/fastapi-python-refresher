8ff341c6a24145bc3eccbe88ae30d1825a6c68e0
knarfortiz <o.g.knarf@hotmail.com>
2024-10-16 08:47:31 -0500

    list

---
+++ List.py
@@ -0,0 +1,13 @@
+"""
+List are a collection of data
+"""
+
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+
+people_list = ['John', 'Jane', 'Jack']
+print(people_list)
+print(people_list[0:2])
+
+people_list.append('Jill')
+print(people_list)
